e7f8c562c18cf7707ed1922ab1f6482881df092e
AntigenDecorations <327495181+antigendecorations@users.noreply.github.com>
2026-09-10 23:28:28 +0700

    draft2

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ function exportElementAsImage(element) {
 		element = document.getElementById('save-image-target');
 	}
 	
+	console.log('exportElementAsImage', element);
+	// await document.fonts.ready; 
+	
 	html2canvas(element, {
 		width: element.offsetWidth,
 		height: element.offsetHeight,
-		scale: 1 // Ensures 1:1 pixel mapping without standard high-DPI scaling
+		scale: 1, // Ensures 1:1 pixel mapping without standard high-DPI scaling
+		scrollX: 0,
+		scrollY: 0,
 	}).then(canvas => {
 		const link = document.createElement('a');
 		link.download = 'Domestication contract.png';
